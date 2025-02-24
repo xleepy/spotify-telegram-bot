b3384b4911b39bad1083b2ad5c58452a9da45d0c
xleepy <ragux0@gmail.com>
2025-02-24 12:43:20 +0100

    quickfix unhandled rejection for now

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-require('dotenv').config();
-const { randomUUID } = require('crypto');
+require("dotenv").config();
+const { randomUUID } = require("crypto");
 
-const TelegramBot = require('node-telegram-bot-api');
-const spotify = require('./spotify');
+const TelegramBot = require("node-telegram-bot-api");
+const spotify = require("./spotify");
 
-function parseQuery(query = '') {
-  const urlArr = query.split('/');
+function parseQuery(query = "") {
+  const urlArr = query.split("/");
   if (urlArr.length === 0) {
     return null;
   }
   // node 16.1.0 on windows doesn't support at function
   const type = urlArr[urlArr.length - 2];
-  const [id] = urlArr[urlArr.length - 1].split('?');
+  const [id] = urlArr[urlArr.length - 1].split("?");
   return {
     id,
     type,
@@ -20,18 +20,18 @@ function parseQuery(query = '') {
 
 function createMessageText(...arr) {
   if (arr.length === 0) {
-    return 'Not found';
+    return "Not found";
   }
-  return (arr || []).filter(Boolean).join('\n');
+  return (arr || []).filter(Boolean).join("\n");
 }
 
-function createArticle(url, name, thumb, messageText = '') {
+function createArticle(url, name, thumb, messageText = "") {
   return {
     id: randomUUID(),
-    type: 'article',
+    type: "article",
     input_message_content: {
       message_text: messageText,
-      parse_mode: 'HTML',
+      parse_mode: "HTML",
     },
     title: name,
     url,
@@ -44,14 +44,24 @@ function createArticle(url, name, thumb, messageText = '') {
 
 function makeArticleByType(type, data) {
   const url = data.external_urls.spotify;
-  if (type === 'playlist') {
+  if (type === "playlist") {
     const { name, images = [] } = data;
-    return createArticle(url, name, images[0], createMessageText(`*${name}*`, `[Spotify](${url})`));
+    return createArticle(
+      url,
+      name,
+      images[0],
+      createMessageText(`*${name}*`, `[Spotify](${url})`)
+    );
   }
   const { name, images = [], artists = [], album } = data;
-  const image = type === 'track' ? album.images[0] : images[0];
-  const fullName = artists.length > 0 ? `${artists.map(a => a.name).join(', ')}: ${name}` : name;
-  const youtubeUrl = `https://www.youtube.com/results?search_query=${encodeURIComponent(fullName)}`;
+  const image = type === "track" ? album.images[0] : images[0];
+  const fullName =
+    artists.length > 0
+      ? `${artists.map((a) => a.name).join(", ")}: ${name}`
+      : name;
+  const youtubeUrl = `https://www.youtube.com/results?search_query=${encodeURIComponent(
+    fullName
+  )}`;
   return createArticle(
     url,
     fullName,
@@ -66,12 +76,13 @@ function makeArticleByType(type, data) {
 }
 
 (async () => {
-  const { getTrackInfoById, getAlbumInfoById, getPlaylistById } = await spotify();
+  const { getTrackInfoById, getAlbumInfoById, getPlaylistById } =
+    await spotify();
   const bot = new TelegramBot(process.env.API_TOKEN, {
     polling: true,
   });
 
-  bot.on('inline_query', async ({ query, id }) => {
+  bot.on("inline_query", async ({ query, id }) => {
     if (query.length === 0) {
       return;
     }
@@ -82,17 +93,21 @@ function makeArticleByType(type, data) {
     const { id: spotifyId, type } = parsedQuery;
     const getInfo = () => {
       switch (type) {
-        case 'track':
+        case "track":
           return getTrackInfoById(spotifyId);
-        case 'album':
+        case "album":
           return getAlbumInfoById(spotifyId);
-        case 'playlist':
+        case "playlist":
           return getPlaylistById(spotifyId);
         default:
           return null;
       }
     };
-    const data = await getInfo();
-    bot.answerInlineQuery(id, [makeArticleByType(type, data)]);
+    try {
+      const data = await getInfo();
+      await bot.answerInlineQuery(id, [makeArticleByType(type, data)]);
+    } catch (err) {
+      console.error(err);
+    }
   });
 })();
